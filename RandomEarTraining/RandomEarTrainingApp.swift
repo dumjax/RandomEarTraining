b0ae8fd884cfca37ae86259c8827e31b9a27ad33
Maxime <dumjax@gmail.com>
2024-10-26 16:41:24 +0200

    Initial Commit

---
+++ RandomEarTraining/RandomEarTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomEarTrainingApp.swift
+//  RandomEarTraining
+//
+//  Created by dumjax on 26.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct RandomEarTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
